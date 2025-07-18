6ad461a7ef9e89f539afaa3fea834a6639e24a15
Giribala Arun <120817536+Giga4byte@users.noreply.github.com>
2025-07-18 19:29:10 +0530

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <section className='w-full h-[1px] items-center justify-center text-neutral-600'>
+      < /br>
         <h3 className='text-center font-medium'>
             ⋮ thank you for making it this far and looking at my portfolio. i appreciate it ⋮
         </h3>
