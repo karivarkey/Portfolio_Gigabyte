1945932a2a3f16067928b5c9925f305f630a4eae
Giribala Arun <120817536+Giga4byte@users.noreply.github.com>
2025-07-18 19:33:26 +0530

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className=''w-full py-4 mt-20 text-center text-neutral-500'>
-      < /br>
-        <h3 className='text-center font-medium'>
-            ⋮ thank you for making it this far and looking at my portfolio. i appreciate it ⋮
-        </h3>
+    <footer className="w-full py-4 mt-20 text-center text-neutral-500">
+      <h3 className="font-medium">
+        ⋮ thank you for making it this far and looking at my portfolio. i appreciate it ⋮
+      </h3>
     </footer>
   )
 }
